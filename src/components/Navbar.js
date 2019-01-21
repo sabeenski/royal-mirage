c9f0dc691d86cc489b20770e1f0f338e0ed536b5
sabeenski <sabinadhaugoda@gmail.com>
2019-01-21 02:00:36 +0100

    Hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,22 +4,32 @@ import './Navbar.css'
 
 
 class Navbar extends Component {
-  state = {  }
+  state = { 
+    isBurgerMenuOn: false
+   }
+
+  handleClick = () => {
+    this.setState({
+      isBurgerMenuOn: !this.state.isBurgerMenuOn
+    })
+  }
+
   render() { 
     return ( 
       <div>
-        <div className="navbar">
-          <ul>
-            {/* <li><input type="box" placeholder="Search for jobs" /></li> */}
-            <Link to={'/'}><li>Contact</li></Link>
-            <Link to={'/'}><li>Departments</li></Link>
-            <Link to={'/'}><li>About</li></Link>
-            <Link to={'/'}><li>Jobs</li></Link>
-            <Link to={'/'}><li>Home</li></Link> 
-            <Link to={'/'}><p className="logo">Royal Mirage</p></Link>       
-          </ul>
-        </div>
-        <img src="images/open-jobs-IT.jpg" alt="jobs" width="100%"/>
+          <nav className="navbar">
+              <div id="burger-nav" onClick={this.handleClick}></div>
+              <ul className={this.state.isBurgerMenuOn ? "open" : "close"}>
+                <Link to={'/'}><li>Contact</li></Link>
+                <Link to={'/'}><li>Departments</li></Link>
+                <Link to={'/'}><li>About</li></Link>
+                <Link to={'/'}><li>Jobs</li></Link>
+                <Link to={'/'}><li>Home</li></Link> 
+                <Link to={'/'}><h2 className="logo">Royal Mirage</h2></Link>       
+              </ul>
+            <img src="images/open-jobs-IT.jpg" alt="jobs" width="100%"/>
+          </nav>
+
       </div>
 
     );
@@ -27,3 +37,6 @@ class Navbar extends Component {
 }
  
 export default Navbar;
+
+
+ 
